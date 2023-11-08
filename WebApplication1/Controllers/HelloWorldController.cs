87294ac7670227de4ba1041f9df46d5205cffeb8
Ilya Ershov <ilyaerhov2000@yandex.ru>
2023-11-08 20:23:19 +0300

    Simply changed default web app

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//using System.Text.Encodings.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        // https://localhost:7212/HelloWorld
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        // 
+        // GET: /HelloWorld/Welcome
+        // https://localhost:7212/HelloWorld/Welcome
+        public string Welcome()
+        {
+            String responseStr = "<h1 class=\"display-4\">This is the Welcome action method<br>We from HelloWorld controller</h1>";
+            return responseStr;
+        }
+
+        // View //
+        public IActionResult DemoView()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Controller] // атрибут (необязателен, если суффикс controller или наследуем от класса с суффиксом Controller) 
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ActionName("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [ActionName("Privacy")]
+        public IActionResult PrivacyAttributeExample()
+        {
+            return View();
+        }
+
+        [NonAction]
+        public string MyActionFromGuide() => "Hello METANIT.COM";
+
+        // пример из гайда
+        public async Task GuideRequest()
+        {
+            Response.ContentType = "text/html;charset=utf-8";
+            await Response.WriteAsync("<h2>Hello METANIT.COM</h2>");
+        }
+
+        // пример из гайда Response
+        public async Task GuideResponseRequest()
+        {
+            Response.ContentType = "text/html;charset=utf-8";
+            System.Text.StringBuilder tableBuilder = new("<h2>Request headers</h2><table>");
+            foreach (var header in Request.Headers)
+            {
+                tableBuilder.Append($"<tr><td>{header.Key}</td><td>{header.Value}</td></tr>");
+            }
+            tableBuilder.Append("</table>");
+            await Response.WriteAsync(tableBuilder.ToString());
+        }
+
+        // Гайд, передача параметра
+        public string GuideParameter(string name, int age = 33)
+        {
+            return $"Your name: {name}, Age: {age}";
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
